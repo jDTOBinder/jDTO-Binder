4b96b785f7131b3e5583aba6915c0dd1ba2f6c3a
juancavallotti <juancavallotti@192.168.1.107>
2011-10-23 21:25:41 -0300

    added utility for parsing math expressions.

---
+++ src/main/java/com/juancavallotti/jdto/util/expression/CompoundTerm.java
@@ -0,0 +1,51 @@
+/*
+ *    Copyright 2011 Juan Alberto López Cavallotti
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+package com.juancavallotti.jdto.util.expression;
+
+import java.io.Serializable;
+
+/**
+ * Represents a compound term of an expression. A compound term is of the type
+ * of terms that take two terms and an operator to produce some result.
+ * @author juancavallotti
+ */
+final class CompoundTerm implements ExpressionTerm, Serializable {
+    private static final long serialVersionUID = 1L;
+    
+    private final Operator operator;
+    private final ExpressionTerm leftSide;
+    private final ExpressionTerm rightSide;
+    
+    /**
+     * Create a new unstance of a compond term
+     * @param operator
+     * @param leftSide
+     * @param rightSide 
+     */
+    public CompoundTerm(Operator operator, ExpressionTerm leftSide, ExpressionTerm rightSide) {
+        this.operator = operator;
+        this.leftSide = leftSide;
+        this.rightSide = rightSide;
+    }
+    
+    @Override
+    public double evaluate() {
+        return operator.apply(leftSide, rightSide);
+    }
+    
+}
---
+++ src/main/java/com/juancavallotti/jdto/util/expression/VariableTerm.java
@@ -0,0 +1,68 @@
+/*
+ *    Copyright 2011 Juan Alberto López Cavallotti
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.juancavallotti.jdto.util.expression;
+
+import java.io.Serializable;
+import java.util.HashMap;
+
+/**
+ * Represents a term that is resolved via a table.
+ * @author juancavallotti
+ */
+class VariableTerm implements ExpressionTerm, Serializable {
+    private static final long serialVersionUID = 1L;
+    private final String variableName;
+    private final HashMap<String, Number> variableMap;
+    
+    private final Expression.VariableResolver resolver;
+    
+    /**
+     * Build a new instance of the a variable term.
+     * @param variableName
+     * @param variableMap 
+     */
+    public VariableTerm(String variableName, HashMap<String, Number> variableMap, Expression.VariableResolver resolver) {
+        this.variableName = variableName;
+        this.variableMap = variableMap;
+        this.resolver = resolver;
+    }
+    
+    /**
+     * Null or not found variables will produce a runtime exception.
+     */
+    @Override
+    public double evaluate() {
+        //try to get the value
+        Number value = variableMap.get(variableName);
+        
+        if (value == null && resolver == null) {
+            throw new IllegalArgumentException("No value for variable: "+variableName);
+        }
+        
+        if (value == null) {
+            value = resolver.resolveVariable(variableName);
+        }
+        
+        if (value == null) {
+            throw new IllegalArgumentException("Could not resolve variable: "+variableName);
+        }
+        
+        return value.doubleValue();
+    }
+    
+    
+}
